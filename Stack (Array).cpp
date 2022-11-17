a55c69fa05fd0577e924b82d9a4d473070fb4d2b
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-17 22:11:39 +0500

    Stack (Struct and Constructor)

---
+++ Stack (Array).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+struct stack
+{
+	int stackTop, maxStackSize, item, stackSize, *list;
+	char ask;
+
+	stack ()
+	{
+		cout<<"Please enter the size of the stack."<<endl;
+		cin>>stackSize;
+
+		if (stackSize<=0)
+		{
+			cout<<"Size must be a positive number."<<endl;
+			cout<<"Creating array of 100."<<endl;
+
+			maxStackSize=100;
+		}
+
+		else
+			maxStackSize=stackSize;
+
+		stackTop=0;
+		list=new int [maxStackSize];
+	}
+
+	
+};
+
+void main ()
+{
+	stack obj;
+
+	
+
+
+getch();
+}
