efae4dca3da4188eff410f62e3f53ef275813468
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-17 22:16:44 +0500

    Stack using Array (Struct and Constructor)

--- Stack (Array).cpp
+++ Stack (Array).cpp
@@ -28,106 +28,13 @@ struct stack
 		list=new int [maxStackSize];
 	}
 
-	bool emptyStack ()
-	{
-		return (stackTop==0);
-	}
-
-	bool fullStack ()
-	{
-		return (stackTop==maxStackSize);
-	}
-
-	void push()
-	{
-		ask='y';	
-		while (ask=='y' || ask=='Y')
-		{
-			cout<<"Please enter the value in digits."<<endl;
-			cin>>item;
-
-			if (!fullStack())
-			{
-				list[stackTop]=item;
-				stackTop++;
-			}
-				
-			else
-				cout<<"Stack is full."<<endl;
-		
-			cout<<"If you want to store more digits, enter 'y'."<<endl;
-			cin>>ask;
-		}
-	}
-
-	void pop ()
-	{
-		if (!emptyStack())
-			stackTop--;
-
-		else
-			cout<<"Empty Stack."<<endl;
-	}
-
-	void copy (stack otherStack)
-	{
-		delete [] list;
-		maxStackSize=otherStack.maxStackSize;
-		stackTop=otherStack.stackTop;
-
-		list=new int [maxStackSize];
-
-		for (int i=0; i<maxStackSize; i++)
-			list[i]=otherStack.list[i];
-	}
-
-	void display ()
-	{
-		if (!emptyStack())
-			for (int j=0; j<stackTop; j++)
-				cout<<list[j]<<" ";
-
-		cout<<endl;
-	}
-
-	void menu ()
-	{
-		ask='y';
-		
-		int entry;
-		{
-			while (ask=='y' || ask=='Y')
-			{
-				cout<<"Welcome."<<endl;
-				cout<<"Please enter 1 to Push Stack."<<endl;
-				cout<<"Please enter 2 to Pop Stack."<<endl;
-				cout<<"Please enter 3 to Display Stack."<<endl;
-				cin>>entry;
-
-				if (entry==1)
-					push();
-
-				else if (entry==2)
-					pop();
-
-				else if (entry==3)
-					display();
-			
-				else
-					cout<<"Enter the correct entry."<<endl;
-		
-				cout<<"Do you want to go to the menu?."<<endl;
-				cin>>ask;
-			}
-		}
-	}
 };
 
 void main ()
 {
 	stack obj;
 
-	obj.menu();
+	
 
 
 getch();
