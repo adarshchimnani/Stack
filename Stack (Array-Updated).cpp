e14fd8fc2fefd63ae022a602f797c84d0780fa1f
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-18 12:53:24 +0500

    Stack struct and constructor

--- Stack (Array-Updated).cpp
+++ Stack (Array-Updated).cpp
@@ -25,98 +25,13 @@ struct stack
 		list=new int [maxStackSize];
 	}
 
-	bool emptyStack ()
-	{
-		return (stackTop==0);
-	}
-
-	bool fullStack ()
-	{
-		return (stackTop==maxStackSize);
-	}
-
-	void push(int x)
-	{
-		
-			if (!fullStack())
-			{
-				list[stackTop]=x;
-				stackTop++;
-				cout<<"Stack has been pushed: "<<x<<endl;
-			}
-				
-			else
-				cout<<"Stack is full."<<endl;
-		
-	}
-
-	void pop ()
-	{
-		if (!emptyStack())
-		{
-			cout<<"Stack has been popped: "<<list[stackTop-1]<<endl;
-			stackTop--;
-		}
-		else
-			cout<<"Empty Stack."<<endl;
-	}
-
-	void copy (stack otherStack)
-	{
-		delete [] list;
-		maxStackSize=otherStack.maxStackSize;
-		stackTop=otherStack.stackTop;
-
-		list=new int [maxStackSize];
-
-		for (int i=0; i<maxStackSize; i++)
-			list[i]=otherStack.list[i];
-	}
-
-	void display ()
-	{
-		if (!emptyStack())
-			for (int j=0; j<stackTop; j++)
-				cout<<list[j]<<" ";
-
-		cout<<endl;
-	}
-
-	void peek()
-	{
-		cout<<endl<<"The top element in the stack is: "<<list[stackTop-1]<<endl;
-	}
-
-	void size()
-	{
-		cout<<endl<<"The no. of element is the stack is: "<<stackTop<<endl;
-	}
-
+	
 	
 };
 
 void main ()
 {
-	stack obj(3);
 
-	obj.push(1);
-	obj.push(2);
-
-	obj.pop();
-	obj.pop();
-
-	obj.push(3);
-
-	obj.peek();
-	obj.size();
-
-	obj.pop();
-
-	if(obj.emptyStack())
-		cout<<endl<<"Stack is empty"<<endl;
-
-	else
-		cout<<endl<<"Stack is not empty"<<endl;
 
 
 
