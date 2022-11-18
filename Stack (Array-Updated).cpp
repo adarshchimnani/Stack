70131ddaaa88cdde8296b823f06a3b0ee8e38668
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-18 13:02:27 +0500

    Copy Stack Function

--- Stack (Array-Updated).cpp
+++ Stack (Array-Updated).cpp
@@ -61,7 +61,19 @@ struct stack
 			cout<<"Empty Stack."<<endl;
 	}
 
-	
+	void copy (stack otherStack)
+	{
+		delete [] list;
+		maxStackSize=otherStack.maxStackSize;
+		stackTop=otherStack.stackTop;
+
+		list=new int [maxStackSize];
+
+		for (int i=0; i<maxStackSize; i++)
+			list[i]=otherStack.list[i];
+	}
+
+
 
 	
 };
