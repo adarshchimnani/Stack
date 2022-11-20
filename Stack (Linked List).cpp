e22dceb047df790c0bd0ff53f73f54088bb27b05
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-20 08:55:34 +0500

    Stack struct and constructor

---
+++ Stack (Linked List).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+struct node
+{
+	int info;
+	node *link;
+};
+
+struct stack
+{
+	node *stackTop, *newNode, *temp, *current, *last;
+	int count, item, entry;
+	char ask;
+
+	stack ()
+	{
+		stackTop=NULL;
+	}
+
+
+
+
+
+	
+
+
+
+	
+};	
+
+void main ()
+{
+	stack obj;
+	
+	
+
+getch();
+}
