a3680b11df82234b3eb3ccd9fd9dadbeadf99f1c
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-23 16:41:06 +0500

    Struct for Node

---
+++ Queue Using Stack.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+int push_count=0;
+
+struct node
+{
+	int info;
+	node *link;
+};
+
+
+
+
+
+void main ()
+{
+	
+
+
+
+_getch();
+}
+
